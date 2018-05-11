e3e78114e9447de385da589a772a09a9c5d73fef
李婷 <liting@eloancn.com>
2018-05-11 10:56:36 +0800

    测试用

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,14 @@ const HTMLPlugin = require('html-webpack-plugin')
 
 const webpack = require('webpack')
 const config = {
-	target: 'web',
-	entry: path.join(__dirname, 'src/index.js'),
-	output: {
-	   	filename: 'bundle.js',
-	    path: path.join(__dirname, 'dist')
-	},
-	module:{
-		rules:[
-			{
+    target: 'web',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
                 test: /\.vue$/,
                 loader: 'vue-loader'
             },
@@ -20,42 +19,40 @@ const config = {
                 test: /\.jsx$/,
                 loader: 'babel-loader'
             },
-			{
-				test:/\.css$/,
-				use:[
-					"style-loader",
-					"css-loader"
-				]
-			},
-			{
-				test:/\.styl$/,
-				use:[
-					"style-loader",
-					"css-loader",
-					{
-						loader:"postcss-loader",
-						options:{
-							sourceMap:true
-						}
-					},
-					"stylus-loader"
-				]
-			},
-			{
-                test: /\.(gif|jpg|jpeg|png|svg)$/,
+            {
+                test: /\.css$/,
                 use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            limit: 1024, // 可转 Base64
-                            name: '[name]-image.[ext]'
-                        }
-                    }
+                    "style-loader",
+                    "css-loader"
                 ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    "stylus-loader"
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: "url-loader",
+                    options: {
+                        limit: 1024, // 可转 Base64
+                        name: '[name]-image.[ext]'
+                    }
+                }]
             }
-		]
-	},
-	plugins: [
+        ]
+    },
+    plugins: [
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: isDev ? '"development"' : '"production"'
@@ -65,17 +62,17 @@ const config = {
     ]
 }
 
-if(isDev){
-	config.devtool = '#cheap-module-eval-source-map' // 调试代码
-	config.devServer = {
-		port: 8000,
-		host: '0.0.0.0',
-		overlay: {
-		  errors: true
-		},
-		hot: true
-	}
-	config.plugins.push(
+if (isDev) {
+    config.devtool = '#cheap-module-eval-source-map' // 调试代码
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true
+        },
+        hot: true
+    }
+    config.plugins.push(
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin()
     )
